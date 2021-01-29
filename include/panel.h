247d3fc2284fef14ae5c073f9430c37c46790de8
Josh Johnson <joshj.tx@gmail.com>
2021-01-29 20:58:39 +0100

    .

---
+++ include/panel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// What does this do?
+// It declares the entry of a new gui "object" or tree structure
+namespace omega {
+namespace Panel {
+void Begin();
+void End();
+void BeginChild();
+void EndChild();
+
+}  // namespace Panel
+}  // namespace omega
