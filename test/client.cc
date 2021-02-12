955f8a654f4d5618dc2e1c605ef79f19dab91b9a
siodine <siodine@gmail.com>
2021-02-12 11:42:26 +0100

    .

--- test/client.cc
+++ test/client.cc
@@ -33,6 +33,17 @@ void CL_Draw() {
                      Color(192, 128, 128, 192)); // needs to be relative to prev
     Window::Draw();
   }
+
+  /*
+  **
+  ** OnPress could set a flag is_moving = true, could call a callback for a button, could say highlight this text
+  **
+  ** Shape& shape = Panel::Declare::Shape(50,50,150,150,Color(kColorWhite));
+  ** //IO::OnMouseMove(Panel::mover(shape)); // mover has to take ref shape
+  ** IO::WhenMouseInShape(shape, IO::OnMousePress(Panel::onPress())); // there's a million things you might want to do after a mouse press - but it has its default behavior
+  **
+  **
+       */
 }
 
 void CL_Shutdown() {}
